0d0a289721518a5d0cedfeced848ebee5da252d1
Matthew <nicely.matt@gmail.com>
2015-12-29 09:15:14 -0600

    Removed all but euclidean data sets

--- runcrap-O3.sh
+++ runcrap-O3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare -a array=("burma14" "att48" "eil76" "bier127" "a280" "rd400" "d493" "att532" "d657" "d1655" "rl1889" "u2319" "d2103" "fl3795")
+declare -a array=("eil76" "bier127" "a280" "rd400" "d493" "d657" "d1655" "rl1889" "u2319" "d2103" "fl3795")
 
 for i in "${array[@]}"
 do
